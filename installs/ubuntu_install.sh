32870090bee870389ed710fdaf143ceeeff6a84e
Ranko Liang <rankoliang@gmail.com>
2020-09-07 17:40:52 -0400

    add audio shortcut to i3

--- installs/ubuntu_install.sh
+++ installs/ubuntu_install.sh
@@ -1,3 +1,6 @@
-apt install tmux batcat ripgrep fzf zathura keepassxc steam i3 arandr autorandr light
-# batcat + ripgrep may have issues being installed together in ubuntu 20.04
+apt install tmux batcat fzf zathura keepassxc steam i3 arandr autorandr light pavucontrol
+# Solves bug in ubuntu 20.04 where ripgrep has issues installing with batcat. 
+# https://bugs.launchpad.net/ubuntu/+source/rust-bat/+bug/1868517/comments/32
+apt download ripgrep
+dpkg --force-overwrite -i ripgrep*.deb
 usermod -a -G video $USER # permissions for light
