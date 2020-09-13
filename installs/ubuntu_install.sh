8b295c07d6bdde5737521076116d0e29e3ee3007
Ranko Liang <rankoliang@gmail.com>
2020-09-12 22:00:59 -0400

    add blue light filter (redshift)

--- installs/ubuntu_install.sh
+++ installs/ubuntu_install.sh
@@ -1,6 +1,7 @@
-apt install tmux batcat fzf zathura keepassxc steam i3 arandr autorandr light pavucontrol
+apt install tmux batcat fzf zathura keepassxc steam i3 arandr autorandr light pavucontrol redshift
 # Solves bug in ubuntu 20.04 where ripgrep has issues installing with batcat. 
 # https://bugs.launchpad.net/ubuntu/+source/rust-bat/+bug/1868517/comments/32
 apt download ripgrep
 dpkg --force-overwrite -i ripgrep*.deb
 usermod -a -G video $USER # permissions for light
+curl https://pyenv.run | bash
---
+++ installs/xps-15.sh
@@ -0,0 +1,1 @@
+# Install libinput-gestures
