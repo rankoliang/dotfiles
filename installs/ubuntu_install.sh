a2cf9b2d4327cf468aa11e4ad33771467a062162
Ranko Liang <rankoliang@gmail.com>
2020-09-14 02:26:20 -0400

    add i3blocks

--- installs/ubuntu_install.sh
+++ installs/ubuntu_install.sh
@@ -1,4 +1,17 @@
-apt install tmux batcat fzf zathura keepassxc steam i3 arandr autorandr light pavucontrol redshift
+apt install \
+  tmux \
+  batcat \
+  fzf \
+  zathura \
+  keepassxc \
+  steam \
+  i3 \
+  arandr \
+  autorandr \
+  light \
+  pavucontrol \
+  redshift \
+  i3blocks
 # Solves bug in ubuntu 20.04 where ripgrep has issues installing with batcat. 
 # https://bugs.launchpad.net/ubuntu/+source/rust-bat/+bug/1868517/comments/32
 apt download ripgrep
