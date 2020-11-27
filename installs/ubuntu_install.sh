5226bbb7d0d267cc63831fa6acd51600bc7577c8
Ranko Liang <rankoliang@gmail.com>
2020-11-27 03:58:00 -0500

    Update permissions and paths

--- installs/ubuntu_install.sh
+++ installs/ubuntu_install.sh
@@ -6,32 +6,36 @@ sudo add-apt-repository ppa:kgilmer/speed-ricer -y
 sudo apt update
 sudo apt upgrade
 sudo apt install \
-  neovim \
-  stow \
-  zsh \
-  tmux \
-  bat \
-  fzf \
-  zathura \
-  steam \
-  i3-gaps \
   arandr \
   autorandr \
+  bat \
+  cargo \
+  compton \
+  curl \
+  fd-find \
+  fzf \
+  i3-gaps \
+  i3blocks \
+  most \
+  neovim \
   pavucontrol \
   redshift \
-  i3blocks \
-  compton \
   rxvt-unicode \
-  fd-find \
-  most \
-  curl \
+  steam \
+  stow \
+  tmux \
+  zathura \
+  zsh \
   -y
 # Solves bug in ubuntu 20.04 where ripgrep has issues installing with batcat. 
 # https://bugs.launchpad.net/ubuntu/+source/rust-bat/+bug/1868517/comments/32
 apt download ripgrep
 sudo dpkg --force-overwrite -i ripgrep*.deb
 
-sudo apt install cargo -y && cargo install exa
+cargo install exa
+
+# change default terminal
+sudo update-alternatives --set x-terminal-emulator $(which urxvt)
 
 # UNIVERSAL CTAGS
 # dependencies 
@@ -66,15 +70,17 @@ cd $DOTFILES && stow -vt ~ vim tmux git zsh fd-find i3 redshift urxvt
 rm -f ~/ripgrep_*.deb
 
 # Install nvm and node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.1/install.sh | bash && nvm install 15.3.0 -v
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.1/install.sh | bash && $DOTFILES/nvm.sh && nvm install 15.3.0 -v
 
 # rbenv dependencies
 sudo apt install gcc make libssl-dev libreadline-dev zlib1g-dev libsqlite3-dev -y \
 && git clone https://github.com/rbenv/rbenv.git ~/.rbenv \
-&& PATH="$HOME/.rbenv/bin:$PATH" \
-&& mkdir -p "$(rbenv root)"/plugins \
+&& $DOTFILES/rbenv/rbenv.sh \
+&& mkdir -p $(rbenv root)/plugins \
 && git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build \
 && rbenv install 2.7.2 -v && rbenv global 2.7.2 # install ruby
+
+$DOTFILES/vim/coc-install-extensions.sh
 
 # Install MesloLGS font
 mkdir /tmp/p10k-fonts
@@ -85,6 +91,3 @@ curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%
 curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 mv * ~/.local/share/fonts
 fc-cache -f -v
-
-# change default terminal
-sudo update-alternatives --set x-terminal-emulator $(which urxvt)
