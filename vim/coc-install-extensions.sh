b0c12cc72731f30d1c8fc923c147912e1f0e2951
Ranko Liang <rankoliang@gmail.com>
2020-11-26 23:51:54 -0500

    Add tailwind extension

--- vim/coc-install-extensions.sh
+++ vim/coc-install-extensions.sh
@@ -11,6 +11,6 @@ then
   echo '{"dependencies":{}}'> package.json
 fi
 # Change extension names to the extensions you need
-npm install coc-snippets coc-solargraph coc-json coc-vimlsp coc-vimtex coc-html coc-css coc-prettier coc-eslint coc-tsserver --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+npm install coc-snippets coc-solargraph coc-json coc-vimlsp coc-vimtex coc-html coc-css coc-prettier coc-eslint coc-tsserver coc-tailwindcss --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 npm install -g eslint
 
