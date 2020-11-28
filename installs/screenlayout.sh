6e4c77ab4037d64332c2e8b8ee59a85e287f53f8
Ranko Liang <rankoliang@gmail.com>
2020-11-28 00:47:35 -0500

    Fix screenlayout.sh install script
    Makes the ~/.screenlayout folder if it does not exist.

--- installs/screenlayout.sh
+++ installs/screenlayout.sh
@@ -4,6 +4,7 @@ if [ $1 ]; then
   if [ -f ~/.screenlayout/display.sh ]; then
     echo "~/.screenlayout/display.sh already exists"
   elif [ ! -f ~/.dotfiles/screenlayout/$1 ]; then
+    mkdir -p ~/.screenlayout
     ln -s ~/.dotfiles/screenlayout/$1.sh ~/.screenlayout/display.sh
     echo "Linking $1.sh"
   else
